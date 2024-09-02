aad45fcdfe31f76f9cbcac5f4c6222ea1774824d
janasagar <ancrea007@gmail.com>
2024-09-02 23:06:55 +0530

    update customer class

--- src/main/java/com/demo/project/DemoProject/entity/Customers.java
+++ src/main/java/com/demo/project/DemoProject/entity/Customers.java
@@ -12,70 +12,12 @@ import java.sql.Timestamp;
 public class Customers {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long customer_id;
-
-    private String first_name;
-    private String last_name;
+    private Long customerId;
+    private String firstName;
+    private String lastName;
     private String email;
-    private String phone_number;
+    private String phoneNumber;
     private String address;
-    private Timestamp created_at;
-
-    // Getters and Setters
-    public Long getCustomerId() {
-        return customer_id;
-    }
-
-    public void setCustomerId(Long customer_id) {
-        this.customer_id = customer_id;
-    }
-
-    public String getFirstName() {
-        return first_name;
-    }
-
-    public void setFirstName(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public String getLastName() {
-        return last_name;
-    }
-
-    public void setLastName(String last_name) {
-        this.last_name = last_name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phone_number;
-    }
-
-    public void setPhoneNumber(String phone_number) {
-        this.phone_number = phone_number;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Timestamp getCreatedAt() {
-        return created_at;
-    }
-
-    public void setCreatedAt(Timestamp created_at) {
-        this.created_at = created_at;
-    }
+    private Timestamp createdAt;
 }
 
